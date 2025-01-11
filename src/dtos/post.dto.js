cd9e72fa28b866c16cb06c13edefb66b1c7094ec
chlwldms <132264125+chlwldms@users.noreply.github.com>
2025-01-12 02:23:54 +0900

    implement service that get all reviews depending on the category

--- src/dtos/post.dto.js
+++ src/dtos/post.dto.js
@@ -1,14 +1,26 @@
 export const bodyToPost = (body) => {
-    return {
-        title: body.title,
-        content: body.content,
-        category: body.category,
-        open: body.open
-
-    }
+  return {
+    title: body.title,
+    content: body.content,
+    category: body.category,
+    open: body.open,
+  };
 };
 export const responseFromPost = ({ post }) => {
-    return {
-        post: post
-    }
+  return {
+    post: post,
+  };
+};
+
+export const responseFromAllPosts = (posts) => {
+  const AllPosts = posts.map((post) => ({
+    postId: post.id,
+    title: post.title,
+    content: post.content,
+    category: post.category,
+  }));
+
+  return {
+    success: AllPosts,
+  };
 };
--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,22 +1,27 @@
-import { responseFromPost } from "../dtos/post.dto.js";;
+import { responseFromPost, responseFromAllPosts } from "../dtos/post.dto.js";
 import {
-    addPost,
-    getPost
+  addPost,
+  getPost,
+  getAllPosts,
 } from "../repositories/post.repository.js";
 //import { DuplicateUserEmailError } from "../errors/errors.js";
 
 export const postAdding = async (userId, data) => {
-    const postId = await addPost({
-        userId: userId,
-        title: data.title,
-        content: data.content,
-        category: data.category,
-        open: data.open
-    });
+  const postId = await addPost({
+    userId: userId,
+    title: data.title,
+    content: data.content,
+    category: data.category,
+    open: data.open,
+  });
 
-    const post = await getPost(postId);
-    return responseFromPost(
-        {
-            post
-        });
+  const post = await getPost(postId);
+  return responseFromPost({
+    post,
+  });
+};
+
+export const listPosts = async (category) => {
+  const posts = await getAllPosts(category);
+  return responseFromAllPosts(posts);
 };
